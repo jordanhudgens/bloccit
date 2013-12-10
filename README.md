15edf048bc77e479e0fa8c83fe7bcf493820854a
jordanhudgens <jordan@hudgens.com>
2013-12-10 17:58:59 -0600

    first commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-style application demonstrate foundation programming and development skills.
+
+Made with [Michal Kwiatkowski](https://twitter.com/michalkw)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '355301b3471c53edccd9da3d6400f09be29f87c906c726240ae9ddd378adf567d118997a6445bc3da9611008582c184e785e780571a88c97091cebdfa3ad9b48'
