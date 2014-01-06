abb6f24b106e5cfa0d86459f5feab7c497c49dee
jordanhudgens <jordan@hudgens.com>
2014-01-06 06:12:49 -0600

    added initial destroy function to topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -41,6 +41,18 @@ class TopicsController < ApplicationController
       end
   end
 
+  def destroy
+      @topic = Topic.find(params[:id])
+      name = @topic.name
+      if @topic.destroy
+          flash[:notice] = "\"#{name}\" was deleted successfully."
+          redirect_to topics_path
+      else
+          flash[:error] = "There was an error deleting the topic."
+          render :show
+      end
+  end
+
   private
 
   def topic_params
