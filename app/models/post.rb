16d935c67951bc0fc2754501c38ab9709c3a9222
jordanhudgens <jordan@hudgens.com>
2014-01-10 23:29:49 -0600

    debugging voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,22 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
     belongs_to :user
     belongs_to :topic
 
     mount_uploader :post_image, PostImageUploader
+
+    def up_votes
+        self.votes.where(value: 1).count
+    end
+
+    def down_votes
+        self.votes.where(value: -1).count
+    end
+
+    def points
+        self.votes.sum(:value).to_i
+    end
 
     default_scope { order('created_at DESC') }
 
