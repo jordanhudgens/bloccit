e223f8490c1fe8f1115b59626269fab8c7da976b
jordanhudgens <jordan@hudgens.com>
2014-01-16 21:48:19 -0600

    bug fix for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :posts
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
