7f9c4667811afc9755ed2d56128f56b2061dbde3
jordanhudgens <jordan@hudgens.com>
2014-01-25 21:29:36 -0600

    fixed ajax bug for destroying comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,10 +30,8 @@ class CommentsController < ApplicationController
 
       if @comment.destroy
           flash[:notice] = "Comment was removed"
-          redirect_to [@topic, @post]
       else
           flash[:error] = "Comment couldn't be deleted, please try again"
-          redirect_to [@topic, @post]
       end
 
       respond_with(@comment) do |f|
